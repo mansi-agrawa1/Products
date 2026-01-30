8f9cdc8b7abb4d52f615a99bc99d844701821df3
mansi.agrawal <mansi.agrawal@innovationm.com>
2026-01-30 21:38:56 +0530

    created CRUD APIs using viewset

---
+++ inventory/inventory_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet
+
+router = DefaultRouter()
+router.register('items', ProductViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
