0f32aa63b8382feaadfa793fa937bd3eddd1e2d2
mansi.agrawal <mansi.agrawal@innovationm.com>
2026-01-30 22:15:21 +0530

    added pdf and excel apis

--- inventory/inventory_app/urls.py
+++ inventory/inventory_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductViewSet, export_to_excel, export_to_pdf
 
 router = DefaultRouter()
 router.register('items', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('items/export/pdf/', export_to_pdf),
+    path('items/export/excel/', export_to_excel),
+
 ]
--- inventory/inventory_app/views.py
+++ inventory/inventory_app/views.py
@@ -2,10 +2,60 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product
 from .serializer import ProductSerializer
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
+from openpyxl import Workbook
+
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-# Create your views here.
+
+
+def export_to_pdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+
+    c = canvas.Canvas(response)
+    y = 800
+    c.drawString(100, y, "Inventory Report")
+    products = Product.objects.all()
+    for item in products:
+        text = f"ID: {item.id}, Name: {item.name}, Qty: {item.quantity}, Price: {item.price}, Created: {item.created_at.strftime('%Y-%m-%d')}"
+        y -= 20
+        c.drawString(100, y, text)
+        if y < 50:
+            c.showPage()
+            y = 800
+
+    c.save()
+    return response
+
+
+
+def export_to_excel(request):
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Items Report"
+
+    ws.append(["ID", "Name", "Quantity", "Price", "Created At"])
+
+    items = Product.objects.all()
+    for item in items:
+        ws.append([
+            item.id,
+            item.name,
+            item.quantity,
+            float(item.price),
+            item.created_at.strftime('%Y-%m-%d')
+        ])
+
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = 'attachment; filename=items_report.xlsx'
+
+    wb.save(response)
+    return response
